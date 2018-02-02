9623b2a2e0ba9b47884e67e55967bf6a135b0198
angelmer <maximer@mail.ru>
2018-02-02 20:13:09 +0300

    create modal

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,34 @@
-$(function(){
 
+var modal = document.getElementById("modal");
+var sizeBtn = document.getElementById("btn");
+var closeModal = document.getElementById ("close");
+var overlay = document.getElementById("overlay");
+
+    sizeBtn.addEventListener("click", function(event){
+        event.preventDefault();
+        modal.classList.add("modal__calculate--open");
+        overlay.classList.add("overlay--open");
+    });
+
+closeModal.addEventListener("click", function(event){
+    event.preventDefault();
+    modal.classList.remove("modal__calculate--open");
+    overlay.classList.remove("overlay--open");
+});
+
+overlay.addEventListener("click", function(event){
+    event.preventDefault();
+    modal.classList.remove("modal__calculate--open");
+    overlay.classList.remove("overlay--open");
+});
+
+window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+        modal.classList.remove("modal__calculate--open");
+        overlay.classList.remove("overlay--open");
+        if (modal.classList.contains("modal__calculate--open")) {
+            modal.classList.remove("modal__calculate--open");
+            overlay.classList.remove("overlay--open");
+        }
+    }
 });
